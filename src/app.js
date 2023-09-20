d6ae45f3bb5dee11723876d3dba84feafe2cd434
juligon <julianagon73@gmail.com>
2023-09-20 15:01:36 -0300

    fixing bugs

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const cors = require("cors");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const routes = require("./routes/index.js");
 const morgan = require("morgan");
 const path = require("path");
@@ -28,16 +29,32 @@ const swaggerSpec = {
  	apis: [`${path.join(__dirname, "./routes/*.js")}`],
 };
 
-server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
 server.use(morgan("dev"));
 server.use(
  	"/api-doc",
  	swaggerUI.serve,
  	swaggerUI.setup(swaggerJsDoc(swaggerSpec))
 );
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+	res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 server.use("/api", routes);
+
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
 module.exports = server;
